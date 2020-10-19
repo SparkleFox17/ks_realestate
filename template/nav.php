11593ba0c24414788830b9d06389ba62f815169e
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-18 23:40:37 -0400

    nav and sub nav

--- template/nav.php
+++ template/nav.php
@@ -1,19 +1,33 @@
 <div class="nav">
     <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#realestate">Real Estate</a></li>
-        <li><a href="#calculators">calculators</a></li>
-        <li><a href="#photos">Gallery</a></li>
-        <li><a href="#contact">Contact</a></li>  
+        <li>Home</li>
     </ul>
-    <ul class="sub-nav">
-        <li>Investments</li>
-        <li>Buy</li>
-        <li>Sell</li>
-        <li>Mortgage Calculator</li>
-        <li>CMHC Insurance Calculator</li>
-        <li>Land Transfer Tax Calculator</li>
-        <li>Mortgage Affordabilty</li>
-        <li>Contact Form</li>
+    <li>
+        Services
+        <ul>
+            <li>Investments</li>
+            <li>Sell</li>
+            <li>Buy</li>
+        </ul>
+    </li> 
+    <li>
+        Calculators
+        <ul>
+            <li>Mortgage Calculator</li>
+            <li>CMHC Insurance Calculator</li>
+            <li>Land Transfer Tax Calculator</li>
+            <li>Mortgage Affordabilty</li>
+        </ul>
+    </li> 
+    <ul>
+        <li>Gallery</li>
     </ul>
-</div>
+    <ul>
+        <li>Contact</li>
+    </ul>
+    
+
+
+
+
+
