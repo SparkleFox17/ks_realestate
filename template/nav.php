2ca106817078886e1db7d6bb6c61352678b8e3a1
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-18 23:10:43 -0400

    made corrections

--- template/nav.php
+++ template/nav.php
@@ -1,10 +1,9 @@
 <div class="nav">
     <ul>
         <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
         <li><a href="#realestate">Real Estate</a></li>
         <li><a href="#calculators">calculators</a></li>
-        <li><a href="#photos">Photos</a></li>
+        <li><a href="#photos">Gallery</a></li>
         <li><a href="#contact">Contact</a></li>  
     </ul>
     <ul class="sub-nav">
