635275b4b6141b6473bced3cdfe3cb4d2295b0c7
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-18 23:49:10 -0400

    nested nav

--- template/nav.php
+++ template/nav.php
@@ -1,30 +1,31 @@
 <div class="nav">
     <ul>
         <li>Home</li>
-    </ul>
     <li>
         Services
-        <ul>
-            <li>Investments</li>
-            <li>Sell</li>
-            <li>Buy</li>
-        </ul>
-    </li> 
-    <li>
-        Calculators
-        <ul>
-            <li>Mortgage Calculator</li>
-            <li>CMHC Insurance Calculator</li>
-            <li>Land Transfer Tax Calculator</li>
-            <li>Mortgage Affordabilty</li>
-        </ul>
-    </li> 
-    <ul>
-        <li>Gallery</li>
+            <ul>
+                <li>Investments</li>
+                <li>Sell</li>
+                <li>Buy</li>
+            </ul>
+        </li> 
+        <li>
+            Calculators
+            <ul>
+                <li>Mortgage Calculator</li>
+                <li>CMHC Insurance Calculator</li>
+                <li>Land Transfer Tax Calculator</li>
+                <li>Mortgage Affordabilty</li>
+            </ul>
+        </li> 
+        <li>
+            Gallery
+        </li>
+        </li>
+            Contact
+        </li>
     </ul>
-    <ul>
-        <li>Contact</li>
-    </ul>
+</div>
     
 
 
