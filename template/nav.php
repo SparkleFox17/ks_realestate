970f3fdc79a58bcec88eaa11855bf9ce644df05d
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-18 22:25:41 -0400

    starting navigation

---
+++ template/nav.php
@@ -0,0 +1,20 @@
+<div class="nav">
+    <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#realestate">Real Estate</a></li>
+        <li><a href="#calculators">calculators</a></li>
+        <li><a href="#photos">Photos</a></li>
+        <li><a href="#contact">Contact</a></li>  
+    </ul>
+    <ul class="sub-nav">
+        <li>Investments</li>
+        <li>Buy</li>
+        <li>Sell</li>
+        <li>Mortgage Calculator</li>
+        <li>CMHC Insurance Calculator</li>
+        <li>Land Transfer Tax Calculator</li>
+        <li>Mortgage Affordabilty</li>
+        <li>Contact Form</li>
+    </ul>
+</div>
