79d1babc10f2dfc440aa4ec8dcdd2cc4abdc190d
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-20 13:42:19 -0400

    started arrow

--- view/home.php
+++ view/home.php
@@ -1,2 +1,20 @@
-hello world!
+<div class="landing">
+    <div class="hb-img">
+        <img src="./imgs/hb.jpg" width="" height="">
+    </div>   
+    <p><i class="arrow-down"></i></p>
+</div>
+
+
+
+
+<!--social
+  <div class="social">
+            <img src="./imgs/hb.jpg" width="" height="">
+            <i class="far fa-envelope"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-facebook-square"></i>
+        </div> -->
+
+
 
