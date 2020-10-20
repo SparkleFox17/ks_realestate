258d1fad73011e897c08f42ccdedb072229c1e36
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-20 14:54:04 -0400

    about pg started

--- view/home.php
+++ view/home.php
@@ -4,17 +4,45 @@
     </div>   
     <p><i class="arrow-down"></i></p>
 </div>
-
-
-
-
-<!--social
+<!--social add later
   <div class="social">
             <img src="./imgs/hb.jpg" width="" height="">
             <i class="far fa-envelope"></i>
             <i class="fab fa-instagram"></i>
             <i class="fab fa-facebook-square"></i>
-        </div> -->
+    </div> social add later-->
+
+  <!--sec-2 about ks starts-->
+  <div id="sec-2">
+      <div class="about-ks">
+        <div class="about">
+          <h4>About Kevin Stoddart</h4>
+        </div>
+        <div class="fade-in">
+          <div class="row">
+            <img src="./imgs/fade-in.jpg" width="" height="" />
+            <div class="aboutcontent">
+              <p>
+                *EDIT*<br />
+                Born and raised in Etobicoke, I am now investing and raising my
+                own family here.<br />
+                I can`t imagine a better place to live and work.<br />
+                From my personal investment experience in residential real
+                estate, I have developed a wealth of knowledge. I am able to
+                advise my clients in home remodelling, financing and property
+                management. I strive to provide more knowlege and more service
+                at every oppurtunity.<br />
+                Unlike most people who hold a real estate sales licence in the
+                GTA, real estate is my full time job. I am able to put in the
+                hours needed to provide my clients with the personal and
+                professional service they deserve. <br />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--sec-2 about ks ends-->   
 
 
 
