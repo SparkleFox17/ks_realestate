3ce6e161f5dd2a5a256a079d9234245c13d22f83
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-21 19:19:10 -0400

    updated footer info

--- template/footer.php
+++ template/footer.php
@@ -1,51 +1,29 @@
-  <footer>
+ <footer>
     <div>
         <p class="copyright">&copy 2020 Kevin Stoddart. All Rights Reserved.</p>
-            <div class="n-items">
-                <div class="footer-header">
-                    Website Links
-                    <ul>  
-                        <li>
-                        <a href="./pages/home"></a>  
-                        <a href="./pages/services"></a>  
-                        <a href="./pages/Calculators"></a>  
-                        <a href="./pages/Gallery"></a>  
-                        <a href="./pages/Contact"></a>                  
-                    </ul> 
-                </div>
-            </div> 
+            <div class="nav-items">
+                <div class="footer-li">
+                <h2>SERVICES</h2>
+                    <div>
+                        <ul>  
+                            <li>home</li>
+                            <li>services</li> 
+                            <li>calculators</li> 
+                            <li>gallery</li>  
+                            <li>contact</li>          
+                        </ul> 
+                    </div>
             <div class="footer-social">
-                <div class="footer-header">
-                    Social Media
-                </div>
-                    <div class="row-social">
+                    <h2>Social Media</h2>
+                        <div class="row-social">
                         <i class="far fa-envelope"></i>
                         <i class="fab fa-instagram"></i>
                         <i class="fab fa-facebook-square"></i>
-                    </div>  
-                </div> 
-            </div>       
-            <div class="co-info-container">
-                <div class="kw-re">
-                    <div class="row">
-                        <h2>KW</h2>
-                    </div>
-                    <div class="column">
-                        <p>NEIGHBORHOOD<br>REALTY, BROKERAGE<br>
-                        <p class="lg-kw">KELLERWILLIAMS.<br>
-                        <div class="contentkw-p">*Independently owned and operated</p>
-                        </div>
-                    <div>
-                </div>                
+                    </div> 
+                </div>    
             </div> 
-        </div>     
-  </footer>  
-                
-                
-
-                   
-
-            
+         </div>            
+ </footer>           
 
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script src="<?php echo SITEPATH;?>/js/main.js"></script>
