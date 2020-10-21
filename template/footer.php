52c42b27c788fa3ad577753ae112d385c6b046a5
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-21 12:50:49 -0400

    kw real estate adding

--- template/footer.php
+++ template/footer.php
@@ -1,13 +1,26 @@
   <footer>
-    <p class="copyright">&copy 2020 Kevin Stoddart. All Rights Reserved.</p>
-       
-              
-         
-
-    
-
-
+    <div>
+        <p class="copyright">&copy 2020 Kevin Stoddart. All Rights Reserved.</p>
+            <div class="n-items">
+                <ul>  
+                    <li>Home</li>  
+                    <li>Services</li>
+                    <li>Calculators</li>
+                    <li>Gallery</li>
+                    <li>Contact</li>
+                </ul>
+            </div> 
+            <div class="kw-re">
+                <div class="row">
+                    <h2>KW</h2>
+               </div>
+               <div class="column">
+                    <p>NEIGHBORHOOD<br>REALTY BROKERAGE</p>
+                </div>
+            </div> 
+        </div>     
   </footer>  
+
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script src="<?php echo SITEPATH;?>/js/main.js"></script>
 </body>
