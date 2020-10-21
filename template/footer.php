3536a8a9a3247fbdc8951361f22ab4405d345385
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-21 11:10:20 -0400

    adding content

--- template/footer.php
+++ template/footer.php
@@ -1,25 +1,7 @@
   <footer>
     <p class="copyright">&copy 2020 Kevin Stoddart. All Rights Reserved.</p>
-    <div class="kw-box">
-        <div class="kw-inner-text">
-            <div>
-                <h4 class="KW"></h4>
-                <h4>KW Neighborhood<br>Realty, Brokerage</h4>
-                    <p>
-                        Keller Williams
-                    </p>
-                    <p>
-                        Each office is independently owned and operated.
-                    </p>
-                </div>
-            </div>  
-        </div>
-        <div class="leg-priv">
-            <h4>KW Legal and Privacy</h4>
-            <p>content add</p>
-        </div> 
-    </div>
-
+       
+              
          
 
     
