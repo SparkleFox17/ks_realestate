80c97f7f657a6df96a759575c0f37ae4d2eefa94
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-21 10:03:48 -0400

    footer content. Legal&Privacy & 2020 & KW layout

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,25 @@
   <footer>
-    <p class="copyright">&copy 2020</p>
+    <p class="copyright">&copy 2020 Kevin Stoddart. All Rights Reserved.</p>
+
+    <div class="kw-box">
+        <div class="kw-inner-text">
+            <div>
+                <h4>KW</h4>
+                    <p>content</p>
+                </div>
+            </div>  
+        </div>
+        <div class="leg-priv">
+            <h4>KW Legal and Privacy</h4>
+            <p>content add</p>
+        </div> 
+    </div>
+</div>
+         
+
+    
+
+
   </footer>  
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script src="<?php echo SITEPATH;?>/js/main.js"></script>
