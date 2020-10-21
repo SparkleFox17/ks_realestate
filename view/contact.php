ab9769d21f61a0f3c458ab54687a53d0d66ab1ae
apanemia <andrewgphillips74@gmail.com>
2020-10-21 00:56:48 -0400

    contact page and scss cleanup

--- view/contact.php
+++ view/contact.php
@@ -1,7 +1,21 @@
 <div id="contact">
     <div class="page">
-        <div class="content">
-            <p>dynamic contact form</p>
+        <div class="write-up">
+            <div class="content">
+                <h1>Contact Kevin</h1>
+                <p>
+                    I would be so happy to connect with you.
+                    <br/>
+                    Fill in a bit of info and I will be sure to get back to you as soon as possible.
+                    <br/><br/>  
+                    OR
+                    <br/><br/>
+                    Call or text: 416-837-6640<br/>
+                    Email: <a href="mailto:kevin@kevinstoddart.com">kevin@kevinstoddart.com</a>
+                </p>
+            </div>
+        </div>
+        <div class="contact-form">
         </div>
     </div>
 </div>
