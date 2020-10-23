e2d28f270925b2e273f57b2a5703113a1ae0441b
Stephanie Tait <stephanie@Stephanies-MBP.Home>
2020-10-23 17:36:03 -0400

    added footer link-items

--- template/footer.php
+++ template/footer.php
@@ -3,23 +3,47 @@
         <div class="n-items">
             <div class="footer-header">
                 Website Links
-                <ul>  
-                    <li>
-                        <a href="<?php echo SITEPATH; ?>/">Home</a>  
-                    </li>
-                    <li>
-                        <a href="<?php echo SITEPATH; ?>/services/invest">Services</a> 
-                    </li>
-                    <li> 
-                        <a href="<?php echo SITEPATH; ?>/calculators">Calculators</a> 
-                    </li>
-                    <li> 
-                        <a href="<?php echo SITEPATH; ?>/gallery">Gallery</a> 
-                    </li>
-                    <li> 
-                        <a href="<?php echo SITEPATH; ?>/contact">Contact</a> 
-                    </li>                 
-                </ul> 
+                <div class="nav">
+                    <ul class="nav-ul">
+                        <li class="main-item dropdown">
+                            <p class="dropbtn">Buy</p>
+                            <ul class="dropdown-content">
+                                <li><a href="<?php echo SITEPATH;?>/buy/buyers-guide">Buyer's Guide</a></li>
+                                <li><a href="<?php echo SITEPATH;?>/buy/first-time-buyers">First Time Buyers</a></li>
+                                <li><a href="<?php echo SITEPATH;?>/buy/rrsp-home-buyers-plan">RRSP Home Buyer's Plan</a></li>
+                                <li><a href="<?php echo SITEPATH;?>/buy/land-transfer-tax">Land Transfer Tax</a></li>
+                                <li><a href="<?php echo SITEPATH;?>/buy/hst-new-housing-rebate">HST New Housing Rebate</a></li>
+                            </ul>
+                            <div class="clear"></div>
+                        </li> 
+                        <li class="main-item dropdown">
+                            <p class="dropbtn">Sell</p>
+                            <ul class="dropdown-content">
+                                <li><a href="<?php echo SITEPATH;?>/sell/sellers-guide">Seller's Guide</a></li>
+                                <li><a href="<?php echo SITEPATH;?>/sell/free-home-evaluation">Free Home Evaluation</a></li>
+                            </ul>
+                            <div class="clear"></div>
+                        </li> 
+                        <li class="main-item dropdown">
+                            <p class="dropbtn">Invest</p>
+                            <ul class="dropdown-content">
+                                <li><a href="<?php echo SITEPATH;?>/invest/investors-guide">Investor's Guide</a></li>
+                                <li><a href="<?php echo SITEPATH;?>/invest/first-time-investors">First Time Investors</a></li>
+                            </ul>
+                            <div class="clear"></div>
+                        </li> 
+                        <li class="main-item dropdown">
+                            <p class="dropbtn">Calculators</p>
+                            <ul class="dropdown-content">
+                                <li><a href="<?php echo SITEPATH;?>/calculators/mortgage-calculator">Mortgage Calculator</a></li>
+                                <li><a href="<?php echo SITEPATH;?>/calculators/cmhc-insurance-calculator">CMHC Insurance Calculator</a></li>
+                                <li><a href="<?php echo SITEPATH;?>/calculators/land-transfer-tax-calculator">Land Transfer Tax Calculator</a></li>
+                                <li><a href="<?php echo SITEPATH;?>/calculators/mortgage-affordability-calculator">Mortgage Affordabilty</a></li>
+                            </ul>
+                            <div class="clear"></div>
+                        </li> 
+                    </ul>
+                </div>
             </div>
         </div> 
         <div class="footer-social">
@@ -35,7 +59,8 @@
         <div class="co-info-container">
             <div class="kw-re">
                 <div class="row">
-                    <h2></h2>
+                    <h2>KW</h2>
+
                 </div>
                 <div class="column">
                     <p>
