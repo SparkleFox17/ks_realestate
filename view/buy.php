7df3438c4210095910e3e2d0fd4c36228ca24ec9
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-26 21:37:56 -0400

    completed buyer's content

--- view/buy.php
+++ view/buy.php
@@ -59,22 +59,37 @@
                 else if ($service === "first-time-buyers") {
                     ?>
                      <h1>Buying a Home</h1>
-                    <p>kbwurhgwgrihwuguwhgu hvowviuwvwv hovwhiowh</p>
+                    <p>Buying a house for the first time is a big and important decision in your life. As someone who represents you and looks out for your best interest, I would like to introduce you to the First-Time Home Buyers’ Tax Credit.
+                    First-Time Home Buyer’s Credit (FTHBC) is a government program geared towards buyers like you. The objective is to reduce costs associated with purchasing a home. As a first time buyer, you are eligible for a 15% income tax credit on a maximum of $5,000 of home purchase costs such as legal fees and land transfer taxes. It can be claimed for the taxation year that you acquired the house.
+                    You are considered to be a first time buyer if neither you and your spouse or common-law partner has owned and lived in another home in the year of the purchase or in any of the four previous years.</p>
                     <?php  
                 }
                 else if ($service === "rrsp-home-buyers-plan") {
                     ?>
-                    <!--pg1-->
+                    <h1>RRSP Home Buyers Plan</h1>
+                    <p>The Home Buyer’s Plan (HBP) is a government program that allows home buyers to withdraw up to $25,000 from their Registered Retirement Saving’s Plan (RRSP) to put towards buying or building a new home.
+                    Before you can withdraw funds, you have to be entered into a written agreement that specifies whether you will be buying or building your new home.  You also must occupy this space no later than a year after buying or building this new home.
+                    To take advantage of this program you have to be considered a first-time buyer. You are not a first-time home buyer if you (or you and your spouse) has owned a home which you have occupied as your principal place of residence in the past 5 years.
+                    Once those conditions are met each person can withdraw up to $25,000 tax-free from their RRSP for a new home. Couples, including common-law, can withdraw up to $50,000. You will not pay income tax on these amounts as long as these funds will be repaid into an RRSP in the future.
+                    Existing homeowners can also use this program to purchase an accessible home or a home for a disabled dependent relative. The intended individual must qualify for the Disability Tax Credit (DTC) and this home must be more accessible or better suited to the care of this individual. </p>
                     <?php  
                 }
                 else if ($service === "land-transfer-tax") {
                     ?>
-                 
+                    <h1>Land Transfer Tax Rebates</h1>
+                    <p>First-time buyers of new and re-sale homes are eligible for rebates on provincial and Toronto land transfer taxes. The maximum that first-time home buyers can receive for provincial land transfer tax (LTT) is $2,000 and for the Toronto LTT its $3,725.
+                    The provincial LTT is payable anywhere in Ontario, including Toronto. As mentioned before, the maximum that a first-time buyer can receive for the provincial LTT is $2,000. For re-sale homes, the provincial rebate only applies to first-time buyers who entered into Agreements of Purchase and Sale after December 13, 2007.
+                    Toronto LTT is payable only for properties in the City of Toronto. The maximum a first-time home buyer can receive is $3,725. The Toronto LTT rebates are in addition to any provincial LTT rebate the buyer qualifies for.
+                    The buyer is considered to be a first-time buyer for the provincial and Toronto LTT if they are at least 18 years of age and must not have previously owned a home anywhere in the world at anytime. If a spouse is involved, the spouse cannot have owned a home or had any ownership interest in a home while he/she was the purchaser's spouse.</p>
                     <?php  
                 }
                 else if ($service === "hst-new-housing-rebate") {
                     ?>
-                    <!--pg1-->
+                    <h1>HST New Housing Rebate</h1>
+                    <p>The HST New Housing Rebate program allows you to claim a rebate on part of the HST you pay on the purchase price of either a newly constructed home or cost of building the home. You can claim a rebate on a newly renovated home, a purchase of a new mobile home, or when your home is destroyed in a fire and is subsequently rebuilt.
+                    The purchase price of resale homes are exempted from the HST whereas the purchase price of newly constructed homes are subject to HST.
+                    New home buyers can apply for a 36% rebate on the federal portion of the HST of the purchase price to a maximum of $6,300 for homes costing $350,000 or less. For new homes priced at $350,000 to $450,000 the rebate will be reduced proportionately. Homes priced over $450,000 would not receive a rebate on the federal portion of the HST.
+                    For the provincial portion of the HST, new home buyers can apply for a 75% rebate applicable to the purchase price to a maximum of $24,000.</p>
                     <?php  
                 }
             ?>
