a55f6f82eba955003d59876f867b6391629e5bcc
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-26 21:53:28 -0400

    completed seller's guide and buyer's guide made changes

--- view/buy.php
+++ view/buy.php
@@ -7,18 +7,16 @@
                     <h1>This is my Buyer's Guide</h1>
                     <p>As your personal guide and trustworthy agent, I strive to give you the best of the best. In the years that I’ve spent in the industry,
                     homebuyers have always asked me “What do I need to do when I buy a house?”. I’ve composed a step-by-step guide that organizes the buying process<br>  to make buying a house simple and fun!</p>
-                    <br>
                     <h4>Step 1: Decide to Buy </h4>
-                    <br>
                     <p>Before you even go looking for a home, you have to understand your needs. Think about the type of place you’re looking for.
                      Are you looking for a lot of bedrooms and washrooms for your family?<br> Do you want a pool in the backyard? Do you want to live close to a school or a mall?<br>
                     These are just some of the things you have to consider before start looking for a new home.<br>
                     You also have to familiarize yourself with different types of ownership as well to help narrow down the perfect home.<br>
                     Freehold is when it’s all yours, a condo is when you take care of the inside and pay your fees to an association, and co-op is similar to a condo
                     but you rent a unit in the building and pay maintenance and repair fees. Once you have narrowed down some qualities, its time to go looking for a home!</p>
-                    <br>
+                
                     <h4>Step 2: Hire an Agent</h4>
-                    <br>
+               
                     <p>There are thousands of real estate agents how do you chose one that’s right for you?
                     You have to pick carefully. They will be acting as your representative and you will need someone to look out for your best interests. You will need someone that you can trust and someone who understands what you want.
                     There are a few ways to look for your perfect Realtor, such as:<br> 
@@ -29,9 +27,9 @@
                         <br>
                     They have to be a trained professional who knows your area inside out. A great agent is someone who offers you quality services to help you accomplish your goal.
                     </p>
-                    <br>
+               
                     <h4>Step 3: Find your Home</h4>
-                    <br>
+                   
                     <p>There are many ways of finding that dream home! First, before you go shopping you have to make sure you are also financially ready.
                     Make sure to have a budget when you go shopping.<br> You can use my very handy affordability calculator by clicking here.
                     One thing to keep in mind is your down payments. This is a percentage of the total cost of a home that you’ll need to pay.<br>
@@ -39,18 +37,18 @@
                     When the lender approves you for a mortgage, it gives you an idea of how much you can afford to buy your new house.<br>
                     Keeping these things in mind, you are free to go window shopping! You can go searching for houses according to the different neighbourhood surrounding you,<br> 
                     check newspaper listings, websites, or even drop by any open houses<br> that are being held in your area. Looking for houses can be really fun and exciting!</p>
-                    <br>
+                   
                     <h4>Step 4: Make an Offer Now</h4>
-                    <br>
+                 
                     <p>Now that you’ve found the house that you are set on, it’s time to make an offer. Not all offers are the same
                     and here’s where your real estate agent comes in. They will help you with specific terms and conditions that will be specified
                     in the offer like the price you want to pay, financing conditions, or other things like inclusions and exclusions.<br>
                     Not only is it about the price of the house but you have to carefully look at the other details included in this sale.
                     Appliances, chandeliers, or even minor renovations can also be part of the deal.<br> Shorter or longer closing dates can also be specified in the offer.<br>
                     If there is something in the offer that doesn’t satisfy the seller, counter offers or negotiations can also be presented to help get a price that you think is fair.</p>
-                    <br>
+                 
                     <h4>Step 5: Close the Sale</h4>
-                    <br>
+                
                     <p>There will be closing costs associated with the sale that need to be paid either by or on the closing date.<br> 
                     It can include mortgage application fees, inspections, and legal fees. But once that’s taken care of, you get your new house.
                     Congrats, welcome to your new home! You’re all ready to move in!</p>
--- view/sell.php
+++ view/sell.php
@@ -4,7 +4,40 @@
             <?php
                 if ($service === "sellers-guide") {
                     ?>
-                    <!--pg1-->
+                    <h1>Seller's Guide</h1>
+                    <p>Your satisfaction and happiness is something that is very important to me. I understand how hectic it is to sell a house and it can be difficult if it’s not handled properly. To help you through the entire process, I’ve put together five simple steps:</p>
+                    
+                    <h1>Step 1: Decide to Sell</h1> 
+                    <p>When you sell, you want to get as much value as you can for your home. So you might want to consider the timing of the sale because it definitely has an effect on the value. You don’t want to sell when it’s a “buyer’s market,” when too many homes are for sale and there are not enough buyers. Seasonality is something to consider as well because typically more homes are sold in the spring rather than the winter.
+                    You can also increase the value by enhancing the appeal of your house. There are various ways to do this and it all depends on the amount of monetary resources available. You can renovate your house by remodelling an area or just freshen up the walls with a new coat of paint.
+                    Even just keeping your front lawn tidy can make a big difference. Buyers love seeing green grass and flowers outside because it feels more warm and inviting to them.
+                    Home inspections are also something that you can do to prove the value of your home. Buyers will usually ask for a home inspection, so if you do it ahead of time it will definitely impress them. It also gives you a chance to prevent unpleasant surprises and make any major repairs.
+                    Now you’re ready for the for sale sign! </p>
+                    
+                    <h1>Step 2: Hire an Agent</h1>
+                    <p>There are thousands of real estate agents, so how do you chose one that’s right for you?
+                    You have to pick carefully. They will be acting as your representative and you will need someone to look out for your best interests. You will need someone that you can trust and someone who understands what you want.
+                    There are a few ways to look for your perfect Realtor:<br>
+                    <br>
+                    Jot down some names and numbers that you find on “For Sale” signs<br>
+                    Ask friends and family for a recommendation<br>
+                    Visit one of the local offices in your area<br>
+                    <br>
+                    They have to be a trained professional who knows your area inside out. A great agent is someone who offers you quality services to help you accomplish your goal.</p>
+                    
+                    <h1>Step 3: List your Home</h1>
+                    <p>Now that you’ve found the perfect real estate agent, they will list your home. First they will value your house and set a price. A report on market data will be complied to properly value your home based on the prices in your area.
+                    Once they’ve done that the agent will market your home through various media outlets and listing sites to create strong buyer interest. They can post on Multiple Listing Service (MLS), social media, blogs, and websites.
+                    There will be open house appointments as well to showcase the potential of your home to various individuals. At this point, Realtors may also suggest to stage the home to help these individuals imagine themselves living in your house.</p>
+                    
+                    <h1>Step 4: Receive an Offer</h1>
+                    <p>Not all offers are equal and that’s where your real estate agent comes in. They will help you get to know the terms and conditions regarding the price that the buyer wants to pay, financing conditions, or other things like inclusions and exclusions that the buyer wants to make.
+                    Not only is it about the price of the home, but you have to carefully look at the other details included in the sale. Appliances, chandeliers, or even minor renovations can also be part of the deal. Shorter or longer closing dates can also be specified by the buyer.
+                    If there is something in the offer that doesn’t satisfy you, counter offers or negotiations can also be presented to help get you what you want.</p>
+
+                    <h1>Step 5: Close the Sale</h1>
+                    <p>There will be closing costs associated with the sale that need to be paid either by or on the closing date. It can include mortgage application fees, inspections, and legal fees. But once that’s taken care of, you can pass your old keys to the new owners. Congrats!
+                    You’ve officially sold your house!</p>
                     <?php   
                 }
                 else if ($service === "free-home-evaluation") {
