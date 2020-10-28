4d623fcb19f82e7e9710d12c76ea5ef26c3a8de9
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-28 11:52:40 -0400

    fixed about page with img and content side by side

--- view/about.php
+++ view/about.php
@@ -2,22 +2,22 @@
     <div class="page">
       <div class="content"> 
           <img id="about-img" src="./imgs/Kevin-Photo.jpg" width="" height="" />
-          <div>
-          <h4>About Kevin Stoddart</h4>
-            <p>
-              Born and raised in Etobicoke, I am now investing and raising my
-              own family here.<br />
-              I can`t imagine a better place to live and work.<br />
-              From my personal investment experience in residential real
-              estate, I have developed a wealth of knowledge. I am able to
-              advise my clients in home remodelling, financing and property
-              management. I strive to provide more knowlege and more service
-              at every oppurtunity.<br />
-              Unlike most people who hold a real estate sales licence in the
-              GTA, real estate is my full time job. I am able to put in the
-              hours needed to provide my clients with the personal and
-              professional service they deserve. <br />
-            </p>
+          <div class="about-content">
+            <h4>About Kevin Stoddart</h4>
+                <p>
+                    Born and raised in Etobicoke, I am now investing and raising my
+                    own family here.<br />
+                    I can`t imagine a better place to live and work.<br />
+                    From my personal investment experience in residential real
+                    estate, I have developed a wealth of knowledge. I am able to
+                    advise my clients in home remodelling, financing and property
+                    management. I strive to provide more knowlege and more service
+                    at every oppurtunity.<br />
+                    Unlike most people who hold a real estate sales licence in the
+                    GTA, real estate is my full time job. I am able to put in the
+                    hours needed to provide my clients with the personal and
+                    professional service they deserve. <br />
+                </p>
             </div>
           </div> 
         </div>
