c1c4f79af741d999ccee62ed1551678574c70da1
Stephanie Tait <stephanie@Stephanies-MacBook-Pro.local>
2020-10-30 00:00:55 -0400

    corrected content

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 //nav starts
-const nav = document.getElementById('main-nav');
-const navItems = document.querySelectorAll('#main-nav li.main-item');
-const subNavParent = document.getElementsByClassName('has-sub-nav');
-const subNav = document.getElementsByClassName('sub-nav');
+const nav = document.getElementById("main-nav");
+const navItems = document.querySelectorAll("#main-nav li.main-item");
+const subNavParent = document.getElementsByClassName("has-sub-nav");
+const subNav = document.getElementsByClassName("sub-nav");
 
 // mouseover subnav parent
 for (let i = 0; i < subNavParent.length; i++) {
-  subNavParent[i].addEventListener('mouseover', showSubNav);
+  subNavParent[i].addEventListener("mouseover", showSubNav);
 }
 // remove active for normal main items
 for (let i = 0; i < navItems.length; i++) {
-  if (!navItems[i].classList.contains('has-sub-nav')) {
-    navItems[i].addEventListener('mouseover', hideSubNav);
+  if (!navItems[i].classList.contains("has-sub-nav")) {
+    navItems[i].addEventListener("mouseover", hideSubNav);
   }
 }
 // remove active leaving subnav
 for (let i = 0; i < subNav.length; i++) {
-  subNav[i].addEventListener('mouseleave', hideSubNav);
+  subNav[i].addEventListener("mouseleave", hideSubNav);
 }
 
 function showSubNav(e) {
   let el = e.target;
-  let nav = el.parentElement.querySelector('.sub-nav');
-  let allSubNavs = document.querySelectorAll('.sub-nav');
+  let nav = el.parentElement.querySelector(".sub-nav");
+  let allSubNavs = document.querySelectorAll(".sub-nav");
 
   console.log(el, nav);
 
   for (let i = 0; i < allSubNavs.length; i++) {
-    allSubNavs[i].classList.remove('active');
+    allSubNavs[i].classList.remove("active");
   }
 
   if (nav !== null) {
-    nav.classList.add('active');
+    nav.classList.add("active");
   }
 }
 function hideSubNav(e) {
@@ -39,10 +39,14 @@ function hideSubNav(e) {
   console.log(el);
 
   //if (!el.classList.contains('sub-nav')) {
-  console.log('A');
+  console.log("A");
   for (let i = 0; i < subNav.length; i++) {
-    subNav[i].classList.remove('active');
+    subNav[i].classList.remove("active");
   }
   //}
 }
 //nav end
+
+// contact form starts
+
+// contact form ends
